2d7daca0c27076dc2c3e192db426f4b0b9db20e2
mrinal <mrinalg39@gmail.com>
2023-03-27 00:45:09 -0700

    Import language level from database to spinner

--- app/src/main/java/ca/sfu/cmpt276/sudokulang/HomePage2.java
+++ app/src/main/java/ca/sfu/cmpt276/sudokulang/HomePage2.java
@@ -10,12 +10,19 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 
+import java.util.List;
+
+import ca.sfu.cmpt276.sudokulang.data.source.TranslationRepository;
+import ca.sfu.cmpt276.sudokulang.data.source.TranslationRepositoryImpl;
 import ca.sfu.cmpt276.sudokulang.databinding.ActivityHomePage2Binding;
 
 public class HomePage2 extends AppCompatActivity {
-    private Spinner langSpinner, sudokuSpinner;
-    private ArrayAdapter<CharSequence> langAdapter, sudokuAdapter; //only declaration
+    private Spinner langSpinner, sudokuSpinner, mlanglevelSpinner;
+
+    private TranslationRepository mTranslationRepository;
+    private ArrayAdapter<CharSequence> langAdapter, sudokuAdapter, mlanglevelAdapter; //only declaration
 
     /**
      * Create a new intent with the required arguments for {@link HomePage2}.
@@ -38,19 +45,41 @@ public class HomePage2 extends AppCompatActivity {
 
         //------------------------------------------SPINNER INITIALIZATION--------------------------------------------------------
 
-        langSpinner = binding.spinnerLangLevel;
+        mlanglevelSpinner = binding.spinnerLangLevel;
         sudokuSpinner = binding.spinnerSudokuLevel;
 
-        // Populate ArrayAdapter using string array and a spinner layout that we will define
-        langAdapter = ArrayAdapter.createFromResource(this, R.array.array_lang_level, R.layout.spinner_layout);
+        mlanglevelSpinner = findViewById(R.id.spinner_lang_level);
+        mTranslationRepository = TranslationRepositoryImpl.getInstance(getApplicationContext());
+
+        // Initialize the adapter for the spinner
+        mlanglevelAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        mlanglevelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mlanglevelSpinner.setAdapter(mlanglevelAdapter);
+
+
+        // Observe the LiveData returned by the repository
+        mTranslationRepository.getAvailableLanguageLevels().observe(this, new Observer<List<String>>() {
+            @Override
+            public void onChanged(List<String> learningLanguages) {
+                mlanglevelAdapter.clear();
+                mlanglevelAdapter.add(getString(R.string.select_lang_level));
+                mlanglevelAdapter.addAll(learningLanguages);
+                mlanglevelAdapter.notifyDataSetChanged();
+            }
+        });
+
+//        -------------------------------------------------------------------------------------------------------------------------
+
+//        // Populate ArrayAdapter using string array and a spinner layout that we will define
+//        langAdapter = ArrayAdapter.createFromResource(this, R.array.array_lang_level, R.layout.spinner_layout);
         sudokuAdapter = ArrayAdapter.createFromResource(this, R.array.array_sudoku_level, R.layout.spinner_layout);
-
-        // Specify the layout to use when the list of choices appear
-        langAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//        // Specify the layout to use when the list of choices appear
+//        langAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sudokuAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Set adapter to spinner to populate the Lang Spinner
-        langSpinner.setAdapter(langAdapter);
+//
+//        // Set adapter to spinner to populate the Lang Spinner
+//        langSpinner.setAdapter(langAdapter);
         sudokuSpinner.setAdapter(sudokuAdapter);
 
 
@@ -59,10 +88,10 @@ public class HomePage2 extends AppCompatActivity {
         binding.imageButtonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String langLevel = langSpinner.getSelectedItem().toString();
+                String langLevel = mlanglevelSpinner.getSelectedItem().toString();
                 String sudokuLevel = sudokuSpinner.getSelectedItem().toString();
 
-                if (langLevel.contentEquals(langAdapter.getItem(0))
+                if (langLevel.contentEquals(mlanglevelAdapter.getContext().getString(R.string.select_lang_level))
                         || sudokuLevel.contentEquals(sudokuAdapter.getItem(0))) {
                     Toast.makeText(
                             HomePage2.this,

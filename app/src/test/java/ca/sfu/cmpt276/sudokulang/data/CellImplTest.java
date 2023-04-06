1643d5671c33759a31b211391e4f59d3da4fbf34
Hieu D <qhduong@sfu.ca>
2023-04-05 17:18:44 -0700

    Finish test for CellImpl.java

--- app/src/test/java/ca/sfu/cmpt276/sudokulang/data/CellImplTest.java
+++ app/src/test/java/ca/sfu/cmpt276/sudokulang/data/CellImplTest.java
@@ -1,11 +1,13 @@
 package ca.sfu.cmpt276.sudokulang.data;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 class CellImplTest {
-
     @Test
     void getValue() {
         CellImpl cell = new CellImpl();
@@ -16,9 +18,7 @@ class CellImplTest {
     void setValue() {
         CellImpl cell = new CellImpl();
 
-        assertThrows(IllegalArgumentException.class, () -> {
-            cell.setValue(-1);
-        });
+        assertThrows(IllegalArgumentException.class, () -> cell.setValue(-1));
     }
 
     @Test
@@ -37,34 +37,33 @@ class CellImplTest {
     @Test
     void isPrefilled() {
         CellImpl cell = new CellImpl();
-        assertEquals(false, cell.isPrefilled());
+        assertFalse(cell.isPrefilled());
     }
 
     @Test
     void setPrefilled() {
         CellImpl cell = new CellImpl();
         cell.setPrefilled(true);
-        assertEquals(true, cell.isPrefilled());
+        assertThrows(AssertionError.class, cell::isPrefilled);
+        assertThrows(IllegalArgumentException.class, () -> cell.setErrorCell(true));
     }
 
     @Test
     void isErrorCell() {
         CellImpl cell = new CellImpl();
-        assertEquals(false, cell.isErrorCell());//?
+        assertFalse(cell.isErrorCell());
     }
 
     @Test
     void setErrorCell() {
         CellImpl cell = new CellImpl();
         cell.setErrorCell(true);
-        assertEquals(true, cell.isErrorCell());//?
+        assertThrows(AssertionError.class, cell::isErrorCell);
+        assertThrows(IllegalArgumentException.class, () -> cell.setPrefilled(true));
     }
 
     @Test
     void isEmpty() {
-    }
-
-    @Test
-    void testClone() {
+        assertTrue(new CellImpl().isEmpty());
     }
 }
